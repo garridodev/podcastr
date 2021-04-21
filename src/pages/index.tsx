8108c0caa954570d3c12d15e5035cd7d40055404
Emanuel Garrido <emanuel.garrido@hotmail.com>
2021-04-21 11:13:57 -0300

    Criando e ajustando server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,63 @@
-export default function Home() {
+import { useEffect } from "react";
+
+/* Formato SPA */
+// export default function Home() {
+
+  
+//   useEffect(() => {
+//     fetch('http://localhost:3333/episodes')
+//       .then(response => response.json())
+//       .then(data => console.log(data))
+//   }, [])
+
+//   return (
+//     <h1>Index</h1>
+//   );
+// }
+
+//**--------------------------------------------------------**//
+
+/* Formato SSR */
+// export default function Home(props) {
+//   return (
+//     <div>
+//       <h1>Index</h1>
+//       <p>{JSON.stringify(props.episodes)}</p>
+//     </div>
+//   )
+// }
+
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+//**--------------------------------------------------------**//
+
+/* Formato SSG */
+export default function Home(props) {
   return (
-    <h1>Index</h1>
-  );
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
